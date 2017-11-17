7e2f977ccc3ec1451f062ad097ccce108c21c520
irfandre <technocratirfan@gmail.com>
2017-11-18 00:01:49 +0530

    min icon pack

---
+++ MainActivity/gen/min/scratch/iconpack/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package min.scratch.iconpack;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ MainActivity/java/min/scratch/iconpack/MainActivity.java
@@ -0,0 +1,11 @@
+package areeb.scratch.iconpack;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
